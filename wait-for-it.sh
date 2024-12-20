9f2991e5ba462857bffbcb7bc715797a29bb3c79
Abhi <abhisahu98@outlook.com>
2024-12-20 18:39:08 +0530

    changed

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -2,16 +2,14 @@
 
 # wait-for-it.sh - Wait for a service to become available.
 
-TIMEOUT=15
+TIMEOUT=${TIMEOUT:-15}  # Allow overriding timeout using an environment variable
 HOST=$(echo $1 | cut -d':' -f1)
 PORT=$(echo $1 | cut -d':' -f2)
 
 for i in $(seq $TIMEOUT); do
-  nc -z "$HOST" "$PORT" >/dev/null 2>&1
-  if [ $? -eq 0 ]; then
+  if nc -z "$HOST" "$PORT" >/dev/null 2>&1; then
     echo "Service $HOST:$PORT is available!"
-    exec "${@:2}"
-    exit 0
+    exec "${@:2}"  # Execute subsequent commands
   fi
   echo "Waiting for $HOST:$PORT..."
   sleep 1
